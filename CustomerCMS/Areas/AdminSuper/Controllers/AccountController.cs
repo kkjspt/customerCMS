ee59386e00ae57bd59ee116a1494ff8b050b9e2e
胡荣萱 <hxjlo@126.com>
2016-06-10 17:23:40 +0800

    修改了

---
+++ CustomerCMS/Areas/AdminSuper/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using CustomerCMS.Areas.AdminSuper.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerCMS.Areas.AdminSuper.Controllers
+{
+    public class AccountController : Controller
+    {
+        private AccountContext db=new AccountContext();
+        // GET: AdminSuper/Account
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.LoginState = "登录前…";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            string account = fc["account"];
+            string password = fc["password"];
+
+            var user = db.SysUsers.Where(a => a.Email == account & a.Password == password);
+
+            if (user.Count() > 0)
+            {
+                ViewBag.LoginState = "欢迎登录:" + account;
+            }
+            else
+            {
+                ViewBag.LoginState =  account+"__用户不存在！";
+            }
+            return View();
+            //以下代码为跳转实例，不用而已
+            //return RedirectToAction("Login", "Account", new { area = "AdminSuper" });
+        }
+
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
